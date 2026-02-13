fae96e7e590aa1ff64ec54d63d7fe92a65614cee
Eric <bonderic44@gmail.com>
2026-02-13 21:06:12 +0545

    refactor(treesitter): modernize with `opts`, trim parsers, update autotag filetypes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,80 +1,60 @@
 -- Treesitter: syntax highlighting, incremental selection, and text objects
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        enabled = plugin_enabled("treesitter"),
-        event = { "BufReadPre", "BufNewFile" },
-        build = ":TSUpdate",
-        config = function()
-            local treesitter = require("nvim-treesitter.configs")
-
-            treesitter.setup({
-                highlight = {
-                    enable = true,
-                },
-                indent = {
-                    enable = true
-                },
-                ensure_installed = {
-                    "json",
-                    "javascript",
-                    "typescript",
-                    "tsx",
-                    "go",
-                    "yaml",
-                    "html",
-                    "css",
-                    "python",
-                    "http",
-                    "prisma",
-                    "markdown",
-                    "markdown_inline",
-                    "svelte",
-                    "graphql",
-                    "bash",
-                    "lua",
-                    "vim",
-                    "dockerfile",
-                    "gitignore",
-                    "query",
-                    "vimdoc",
-                    "c",
-                    "java",
-                    "rust",
-                    "ron",
-                },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "<C-space>",
-                        node_incremental = "<C-space>",
-                        scope_incremental = false,
-                    },
-                },
-                additional_vim_regex_highlighting = false,
-            })
-        end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    enabled = plugin_enabled("treesitter"),
+    lazy = false,
+    priority = 1000,
+    build = ":TSUpdate",
+    opts = {
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+      ensure_installed = {
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "go",
+        "yaml",
+        "html",
+        "css",
+        "http",
+        "prisma",
+        "markdown",
+        "markdown_inline",
+        "graphql",
+        "bash",
+        "lua",
+        "vim",
+        "dockerfile",
+        "gitignore",
+        "query",
+        "vimdoc",
+        "rust",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+        },
+      },
+      additional_vim_regex_highlighting = false,
     },
-    {
-        "windwp/nvim-ts-autotag",
-        enabled = plugin_enabled("nvim-ts-autotag"),
-        ft = { "html", "xml", "javascript", "typescript", "javascriptreact", "typescriptreact", "svelte" },
-        config = function()
-            require("nvim-ts-autotag").setup({
-                opts = {
-                    enable_close = true,
-                    enable_rename = true,
-                    enable_close_on_slash = false,
-                },
-                per_filetype = {
-                    html = {
-                        enable_close = true,
-                    },
-                    typescriptreact = {
-                        enable_close = true,
-                    },
-                },
-            })
-        end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    enabled = plugin_enabled("nvim-ts-autotag"),
+    ft = { "html", "xml", "javascript", "typescript", "javascriptreact", "typescriptreact", "vue" },
+    opts = {
+      enable_close = true,
+      enable_rename = true,
+      enable_close_on_slash = false, -- close tags when you type </>
     },
+  },
 }
